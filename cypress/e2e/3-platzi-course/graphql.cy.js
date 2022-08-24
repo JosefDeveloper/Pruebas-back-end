f491010a2fc54644b7013fd0af43ef982107f01c
JosefDeveloper <jfelix0107@gmail.com>
2022-08-24 15:44:49 -0500

    Peticiones GraphQL

---
+++ cypress/e2e/3-platzi-course/graphql.cy.js
@@ -0,0 +1,35 @@
+describe("Probando Graphql", function () {
+  it("Debe hacer una consulta con graphql", function () {
+    const gqlQuery = `query pokemons($limit: Int, $offset: Int) {
+        pokemons(limit: $limit, offset: $offset) {
+          count
+          next
+          previous
+          status
+          message
+          results {
+            url
+            name
+            image
+          }
+        }
+      }`;
+
+    const gqlVariables = {
+      limit: 20,
+      offset: 0,
+    };
+
+    cy.request({
+      method: "POST",
+      url: "https://graphql-pokeapi.graphcdn.app",
+      body: {
+        query: gqlQuery,
+        variables: gqlVariables,
+      },
+    }).then((response) => {
+      cy.log(response);
+      expect(response.body.data.pokemons.results[0].name).to.eq("bulbasaur")
+    });
+  });
+});
